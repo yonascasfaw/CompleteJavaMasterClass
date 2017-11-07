1796a2d9ec65a1516cd798844335746a98118243
Yonas Asfaw <yonascasfaw@gmail.com>
2017-11-06 20:47:57 -0800

    KeywordsAndExpressions

---
+++ KeywordsAndExpressions/src/com/yonasasfaw/Main.java
@@ -0,0 +1,12 @@
+package com.yonasasfaw;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int score = 100; // score = 100 is the expression
+        if(score > 99) {  //score > 99
+            System.out.println("You got the high score"); // "you got the high score
+            score = 0; // score = 0
+        }
+    }
+}
