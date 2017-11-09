0bd4dd8120592fbcc3642608c36a359ec0136272
Yonas Asfaw <yonascasfaw@gmail.com>
2017-11-09 13:21:15 -0800

    First lesson on classes

---
+++ Classes/src/com/yonasasfaw/Car.java
@@ -0,0 +1,12 @@
+package com.yonasasfaw;
+
+public class Car {
+
+    private int doors;
+    private int wheels;
+    private String model;
+    private String engine;
+    private String colour;
+
+
+}
---
+++ Classes/src/com/yonasasfaw/Main.java
@@ -0,0 +1,10 @@
+package com.yonasasfaw;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Car porshe = new Car();
+        Car holden = new Car();
+        porshe.
+    }
+}
