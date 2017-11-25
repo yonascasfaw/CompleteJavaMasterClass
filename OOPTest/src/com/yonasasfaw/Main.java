954076f90387b4929adadf8ec2a9e8182deafd44
Yonas Asfaw <yonascasfaw@gmail.com>
2017-11-25 13:46:16 -0800

    Basic burger test

--- OOPTest/src/com/yonasasfaw/Main.java
+++ OOPTest/src/com/yonasasfaw/Main.java
@@ -3,6 +3,12 @@ package com.yonasasfaw;
 public class Main {
 
     public static void main(String[] args) {
-        //challenge problem
+        BasicBurger burger = new BasicBurger("sour dough", "Well done");
+        System.out.println("the price of your basic burger is " + burger.price());
+        //burger.order();
+        burger.additions(true,true,false,true);
+        burger.order();
+
+
     }
 }
