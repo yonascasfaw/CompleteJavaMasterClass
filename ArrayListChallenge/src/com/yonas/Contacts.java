516ae34c95b66913c1f8cee6095085c2af8e2274
yonascasfaw@gmail.com <yonascasfaw@gmail.com>
2017-12-11 00:26:00 -0800

    half way done

--- ArrayListChallenge/src/com/yonas/Contacts.java
+++ ArrayListChallenge/src/com/yonas/Contacts.java
@@ -1,25 +1,59 @@
 package com.yonas;
 
+import java.util.ArrayList;
+
 public class Contacts {
 
-    private String name;
-    private int phoneNumber;
+    ArrayList<Contact> Contacts = new ArrayList<Contact>();
 
-    public Contacts(String name, int phoneNumber) {
-        this.name = name;
-        this.phoneNumber = phoneNumber;
+    public void addContact(String name, int phoneNumber){
+
+        if(findContact(name) < 0){
+            Contact newContact = new Contact(name,phoneNumber);
+            Contacts.add(newContact);
+        }else{
+            System.out.println("Contact already exists");
+        }
+
     }
 
-    public String getName() {
-        return name;
+    public void updateContact(String name, int newNumber){
+
+        int position = findContact(name);
+        if(position >= 0){
+            Contact updatedContact = new Contact(name, newNumber);
+            Contacts.set(position, updatedContact);
+        }else{
+            System.out.println("Contact does not exist.");
+        }
     }
 
-    public int getPhoneNumber() {
-        return phoneNumber;
+    public void removeContact(String name){
+        int position = findContact(name);
+        if(position >= 0){
+            Contacts.remove(position);
+        }else{
+            System.out.println("Contact does not exist.");
+        }
     }
 
-    public void addNewContacts(String name, int phoneNumber){
-        this.name = name;
-        this.phoneNumber = phoneNumber;
+    public void printContacts(){
+        System.out.println("You have " + Contacts.size() + " contacts in you mobile phone.");
+        for(int i = 0; i< Contacts.size(); i++){
+            System.out.println((i+1) + ". " + Contacts.get(i));
+        }
     }
+
+
+
+    public int findContact(String contactName){
+        return Contacts.indexOf(contactName);
+    }
+
+
+//
+//    public void addNewContacts(String name, int phoneNumber){
+//        this.name = name;
+//        this.phoneNumber = phoneNumber;
+//    }
 }
